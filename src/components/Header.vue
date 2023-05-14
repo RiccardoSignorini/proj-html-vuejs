9f657f1fe099e815193a5627136e3c9ca0284a29
RiccardoSignorini <riccardo.signorini1@gmail.com>
2023-05-14 19:03:18 +0200

    dinamicizzazione link

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,28 @@
 
         data(){
             return{
-                link: ["HOME", "ABOUT", "SERVICES", "WORK", "ARTICLES"]
+                link: [
+                    {
+                        name: "HOME",
+                        selected: true
+                    },
+                    {
+                        name: "ABOUT",
+                        selected: false
+                    },
+                    {
+                        name: "SERVICES",
+                        selected: false
+                    },
+                    {
+                        name: "WORK",
+                        selected: false
+                    },
+                    {
+                        name: "ARTICLES",
+                        selected: false
+                    }
+                ]
             }
         }
     }
@@ -20,7 +41,7 @@
         <div class="d-flex justify-content-center align-items-center">
             <ul class="d-flex">
                 <li v-for="(elem, index) in link" :key="index" class="mx-2">
-                    <a href="#">{{ elem }}</a>
+                    <a href="#" :class="(elem.selected==true) ? 'active' : ' ' ">{{ elem.name }}</a>
                 </li>
             </ul>
             <button class="px-4 py-1 mx-3">GET QUOTE</button>
@@ -30,12 +51,12 @@
 
 <style scoped lang="scss">
     #header{
-        height: 80px;
+        height: 100px;
 
         img{
             height: 50px;
         }
-        
+
         li{
             list-style-type: none;
 
@@ -43,11 +64,23 @@
                 text-decoration: none;
                 color: black;
             }
+
+            a:hover{
+                opacity: 0.5;
+            }
+
+            .active{
+                color: #fed23f !important;
+            }
+
+            .active:hover{
+                opacity: 1;
+            }
         }
 
         button{
             background-color: #fed23f;
-            border: #fed23f;
+            border: 0;
         }
     }
 </style>
