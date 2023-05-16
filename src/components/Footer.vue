5a7a0b5ae04b04509e538891267f182328b61d3e
RiccardoSignorini <riccardo.signorini1@gmail.com>
2023-05-16 16:47:46 +0200

    inizio footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,12 +6,37 @@
 
 <template>
     <div id="footer">
-        <div class="container">
+        <div class="container row">
+            <!-- PRIMA COLONNA -->
+            <div id="first-comp" class="col-lg-4 text-start">
+                <img src="/img/footer-logo.png" alt="">
+                <p class="my-4">We are specialists in the construction of unique and exclusive properties. Our work inspires. We pride ourselves on delivering outstanding quality and design for leading clients across the world.</p>
+                <button class="px-5 py-2">LEARN MORE</button>
+            </div>
             
+            <!-- SECONDA COLONNA -->
+            <div>
+
+            </div>
         </div>
     </div>
 </template>
 
 <style scoped lang="scss">
+    #footer{
+        background-color: #31312f;
+        height: 600px;
 
+        #first-comp{
+
+            button{
+                background-color: #fed23f;
+                border: 0;
+            }
+
+            p{
+                color: #6d6d6d;
+            }
+        }
+    }
 </style>
