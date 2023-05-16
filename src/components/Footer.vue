63e883dcb1d7ff13dccd8e41449d7ccdb026ac26
RiccardoSignorini <riccardo.signorini1@gmail.com>
2023-05-16 17:09:28 +0200

    avanzamento footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,22 +1,68 @@
 <script>
     export default{
-        name: "Footer"
+        name: "Footer",
+
+        data(){
+            return{
+                contact: []
+            }
+        }
     }
 </script>
 
 <template>
     <div id="footer">
-        <div class="container row">
+        <div class="container row p-5">
             <!-- PRIMA COLONNA -->
             <div id="first-comp" class="col-lg-4 text-start">
                 <img src="/img/footer-logo.png" alt="">
                 <p class="my-4">We are specialists in the construction of unique and exclusive properties. Our work inspires. We pride ourselves on delivering outstanding quality and design for leading clients across the world.</p>
                 <button class="px-5 py-2">LEARN MORE</button>
             </div>
-            
-            <!-- SECONDA COLONNA -->
-            <div>
 
+            <!-- SECONDA COLONNA -->
+            <div id="second-comp" class="col-lg-4">
+                <h6 class="my-4">LATEST TWEETS</h6>
+                <div class="">
+                    <img src="/img/tweet-avada.png" class="" alt="">
+                </div>
+            </div>
+
+            <!-- TERZA COLONNA -->
+            <div id="thirth-comp" class="col-lg-4">
+                <h6 class="my-4">CONTACT US TODAY</h6>
+                <div>
+                    <ul>
+                        <li>Corporate Location</li>
+                        <li>1600 Amphitheatre Parkway</li>
+                        <li>London WC1 1BA</li>
+                    </ul>
+                    <ul>
+                        <li>Residential Location</li>
+                        <li>9521 Broadsberry Avenue</li>
+                        <li>Paddington RC7 9ZA</li>
+                    </ul>
+                    <ul>
+                        <li>1.800.458.556 / 1.800.532.2112</li>
+                    </ul>
+                    <ul>
+                        <li>info@your-domain.com</li>
+                    </ul>
+                    <ul>
+                        <li>Monday – Friday: 9:00 AM – 6:00 PM</li>
+                    </ul>
+                    <ul>
+                        <li>Saturday – Sunday: 9:00 AM – 12:00 PM</li>
+                    </ul>
+                </div>
+            </div>
+
+            <!-- QUARTA COLONNA -->
+            <div id="fourth-comp" class="col-lg-4">
+                <h6 class="my-4">FIND US</h6>
+                <div>
+                    
+                </div>
             </div>
         </div>
     </div>
@@ -26,6 +72,10 @@
     #footer{
         background-color: #31312f;
         height: 600px;
+        
+        h6{
+                color: #fed23f;
+            }
 
         #first-comp{
 
@@ -37,6 +87,24 @@
             p{
                 color: #6d6d6d;
             }
+        }
+
+        #second-comp{
+
+            img{
+                width: 250px;
+            }
+        }
+
+        #thirth-comp{
+
+            ul{
+                color: #6d6d6d;
+                li{
+                    list-style-type: none;
+                }    
+            }
+            
         }
     }
 </style>
